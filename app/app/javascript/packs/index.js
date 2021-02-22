80f2a943ce267047f939cc5b6c90a04b1d11e20c
Samuel Kerr <transiencymail@gmail.com>
2021-02-22 06:02:42 -0700

    added search feature to products

--- app/app/javascript/packs/index.js
+++ app/app/javascript/packs/index.js
@@ -1,6 +1,21 @@
+import axios from 'axios'
+
 document.addEventListener('DOMContentLoaded',  () => {
- const buttonElement = document.getElementById('searchButton');
- buttonElement.addEventListener('click', function (event) {
- 	alert('foo');
- });
+// let buttonElement = document.getElementById('searchButton');
+// buttonElement.addEventListener('click', function (event) {
+ 	axios.get("http://localhost:3000/products.json")
+ 
+ 		.then(data => {
+ 			for(let datum of data.data){
+ 				console.log(datum)
+ 			}
+
+
+ 		})
+
+
+
+ 		.catch(error => {console.log('ERROR:', error)
+ 	});
+// });
 });
--- app/app/models/product.rb
+++ app/app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+
+
+  def self.search(query)
+    	products = Product.where("name LIKE ?", "%" + query + "%")
+    	if products.count == 0
+    		return Product.all
+    	end
+    	products
+  end
+
 end
